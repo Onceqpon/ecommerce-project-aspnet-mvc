085315481bdb86c978c4320c4244120a0446f8b7
dawid <dawid.pietruszka2002@gmail.com>
2024-01-04 18:28:10 +0100

    Dodanie panelu nawigacji

--- ProjektASPNET/Controllers/ProductsController.cs
+++ ProjektASPNET/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProjektASPNET.Data;
+using ProjektASPNET.Data.Enums;
 using ProjektASPNET.Models;
 
 namespace ProjektASPNET.Controllers
@@ -13,14 +14,42 @@ namespace ProjektASPNET.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             var products = _context.Products
-           .Include(p => p.Manufacturer) // Wczytaj producenta, jeśli jest to wymagane
+           .Include(p => p.Manufacturer)
+           .OrderBy(p => p.Name)
            .ToList();
 
             return View(products);
         }
+
+        public IActionResult IndexByCategory(ProductCategory? category)
+        {
+            int selectedCategoryValue = (int)(category ?? 0);
+
+            var productsInSelectedCategory = _context.Products
+                .Include(p => p.Manufacturer)
+                .Where(p => (int)p.ProductCategory == selectedCategoryValue)
+                .OrderBy(p => p.Name)
+                .ToList();
+
+            return View("Index", productsInSelectedCategory);
+        }
+
+        public IActionResult Search(IFormCollection formCollection)
+        {
+            string searchText = formCollection["search"];
+
+            var productsMatchingSearch = _context.Products
+                .Include(p => p.Manufacturer)
+                .Where(p => p.Name.Contains(searchText) || p.Description.Contains(searchText)) // Szukaj fragmentu w polu Name lub Description
+                .OrderBy(p => p.Name)
+                .ToList();
+
+            return View("Index", productsMatchingSearch);
+        }
+
     }
 }
 
